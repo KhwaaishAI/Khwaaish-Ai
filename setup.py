cf446fa41fdeb11c4fb36d98d90b5449da200f67
Manav-P-ai <manav.p@ironbook.ai>
2025-10-30 12:56:26 +0530

    2

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="Khwaaish",
-    version=0.2,
-    description="A Python package for web automation agents.",
-    packages=find_packages()
-
-)
